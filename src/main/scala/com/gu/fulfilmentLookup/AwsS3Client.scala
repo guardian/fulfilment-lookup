2a31d438b7b88d1f74c4a5b91e2cacdad8204e84
Jacob Winch <jacob.winch@guardian.co.uk>
2017-07-24 17:42:30 +0100

    Re-factor S3 client

--- src/main/scala/com/gu/fulfilmentLookup/AwsS3Client.scala
+++ src/main/scala/com/gu/fulfilmentLookup/AwsS3Client.scala
@@ -1,6 +1,5 @@
 package com.gu.fulfilmentLookup
 
-import java.io.InputStream
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model.GetObjectRequest
 import purecsv.unsafe.CSVReader
@@ -8,7 +7,7 @@ import scala.io.Source
 import scala.util.Try
 
 trait CsvClient {
-  def getDeliveryRows(s3Stream: InputStream): Try[List[DeliveryRow]]
+  def getDeliveryRows(csvString: String): Try[List[DeliveryRow]]
   def getDeliveryRowsFromS3(bucketName: String, subFolder: String, fileName: String): Try[List[DeliveryRow]]
 }
 
@@ -16,21 +15,19 @@ object AwsS3Client extends CsvClient with Logging {
 
   val s3Client = AmazonS3ClientBuilder.defaultClient()
 
-  override def getDeliveryRows(s3Stream: InputStream): Try[List[DeliveryRow]] = {
-    val csvAsString = Source.fromInputStream(s3Stream).mkString
-    val parseAttempt = Try(CSVReader[DeliveryRow].readCSVFromString(csvAsString, skipHeader = true))
-    s3Stream.close()
-    parseAttempt
+  override def getDeliveryRows(csvString: String): Try[List[DeliveryRow]] = {
+    Try(CSVReader[DeliveryRow].readCSVFromString(csvString, skipHeader = true))
   }
 
   override def getDeliveryRowsFromS3(bucketName: String, subFolder: String, fileName: String): Try[List[DeliveryRow]] = {
     val key = s"${subFolder}${fileName}"
     val request = new GetObjectRequest(bucketName, key)
-    val csvStream = Try(s3Client.getObject(request).getObjectContent)
     logger.info(s"Getting file from S3. Bucket: $bucketName | Key: $key")
     for {
-      stream <- csvStream
-      deliveryRows <- getDeliveryRows(stream)
+      csvStream <- Try(s3Client.getObject(request).getObjectContent)
+      csvString <- Try(Source.fromInputStream(csvStream).mkString)
+      _ <- Try(csvStream.close())
+      deliveryRows <- getDeliveryRows(csvString)
     } yield deliveryRows
   }
 
